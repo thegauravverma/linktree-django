496ff26b2fd0324b61a057e0c0637c74a252d0a0
thegauravverma <gauravv0903@gmail.com>
2021-02-22 13:28:39 +0530

    Add link feature added

--- landing_page/views.py
+++ landing_page/views.py
@@ -9,28 +9,37 @@ from django.urls import reverse
 def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
-    return render(request ,"user.html")
+    if request.method == "POST":
+        name = request.POST["name"]
+        url = request.POST["url"]
+        l = Landing(username=request.user, name=name, url=url)
+        l.save()
+        return render(request, "user.html", {
+            "message": "Added Sucessfully"
+        })
+
+    return render(request, "user.html")
 
 
 def login_request(request):
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
-        user = authenticate(request,username=username,password=password)
+        user = authenticate(request, username=username, password=password)
         if user is not None:
-            login(request,user)
+            login(request, user)
             return HttpResponseRedirect(reverse("index"))
         else:
-            return render(request, "login.html",{
-                "message":"Invalid Credential"
+            return render(request, "login.html", {
+                "message": "Invalid Credential"
             })
     return render(request, "login.html")
 
 
 def logout_request(request):
     logout(request)
-    return render(request, "login.html",{
-        "message":"Logged Out"
+    return render(request, "login.html", {
+        "message": "Logged Out"
     })
 
 
